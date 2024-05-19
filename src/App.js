ea4efe24b8958f43cbbbaba0b371f3641d8c843d
Wable Sanket <97028034+WableSanket@users.noreply.github.com>
2024-05-19 21:42:34 +0530

    fixing text

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Connect with me on Linkedin
+          Connect with me
         </a>
       </header>
     </div>
