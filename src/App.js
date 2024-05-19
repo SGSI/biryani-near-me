3f47024b632221608f4d6c51534a49d8e31eee00
Wable Sanket <97028034+WableSanket@users.noreply.github.com>
2024-05-19 21:40:45 +0530

    adding cname to public

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Connect with me
+          Connect with me on Linkedin
         </a>
       </header>
     </div>
